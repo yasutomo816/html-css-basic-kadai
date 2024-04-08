3bada3219c298e24830ef06a9471fefd636961e1
yasutomo816 <135983676+yasutomo816@users.noreply.github.com>
2024-04-08 22:29:30 +0900

    Delete kadai_004 directory

--- kadai_004/event.js
+++
@@ -1,8 +0,0 @@
- $(window).on('load scroll',function(e) {
-    if(e.type === 'load'){
-        console.log('「loadイベントが発生しました」');
-    }
-    if(e.type === 'scroll'){
-        console.log('「scrollイベントが発生しました」');
-    }
-});
