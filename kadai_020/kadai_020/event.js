98c594d77054ab6f23e0b984b144088f00044eb6
yasutomo816 <135983676+yasutomo816@users.noreply.github.com>
2024-04-08 22:30:39 +0900

    Delete kadai_020/kadai_020 directory

--- kadai_020/kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-const text = document.getElementById('text');
-const button = document.getElementById('btn');
-console.log(button)
-button.addEventListener('click', () => {
-    console.log('クリックされました！')
-    text.textContent = 'ボタンをクリックしました'
-});
-
