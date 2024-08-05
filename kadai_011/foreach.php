dac49d3cc8f96415d4110a9612648db8860625a6
yasutomo816 <135983676+yasutomo816@users.noreply.github.com>
2024-08-05 20:36:38 +0900

    Add files via upload

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -10,21 +10,13 @@
      <p>
         <?php
         $personal_date = [
-            'name' => '玉ねぎ', 
-            'price' => '200',
-             'location' => '北海道'
-        ];
-
-        // キーを日本語にマッピング
-        $key_mapping = [
-            'name' => '名前',
-            'price' => '値段',
-            'location' => '産地'
+            '名前' => '玉ねぎ', 
+            '値段' => '200',
+            '産地' => '北海道'
         ];
 
         foreach ($personal_date as $key => $value) {
-            // マッピングを用いて表示
-            echo "{$key_mapping[$key]}: {$value}. <br>";
+            echo "キー:{$key}, 値: {$value} <br>"; 
         }
         ?>
     </p>
